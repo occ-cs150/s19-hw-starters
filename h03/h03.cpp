350aed8aa6d6db097fb48ce80f045f989f31e709
Stephen <occ.computerscience@gmail.com>
2019-01-21 15:40:18 -0800

    Adding h03 starters

---
+++ h03/h03.cpp
@@ -0,0 +1,18 @@
+/**
+ *  @author Put your name here
+ *  @date Put the date here
+ *  @file h03.cpp
+ */
+#include <string>
+using namespace std;
+
+string STUDENT = "WHO AM I?";  // Add your Canvas login name
+
+// Implement your function here
+
+
+/////////////// Optional Student Code /////////////////
+int run()
+{
+    return 0;
+}
