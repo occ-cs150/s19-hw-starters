388af09356657dc0b5df22e330af6dade374cee2
Stephen <occ.computerscience@gmail.com>
2019-02-01 13:21:48 -0800

    h04 and h05 staters added

---
+++ h04/h04.cpp
@@ -0,0 +1,22 @@
+/**
+ *  @author Put your name here
+ *  @date Put the date here
+ *  @file h04.cpp
+ */
+#include <string>
+#include <cctype>
+using namespace std;
+
+string STUDENT = "WHO AM I?";  // Add your Canvas login name
+
+// Write your function here
+
+
+/////////////// Optional Student Code /////////////////
+int run()
+{
+
+    return 0;
+}
+
+
