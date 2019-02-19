28fd5da0f13cb4fc1c89912cc214f4b40d901171
Stephen <occ.computerscience@gmail.com>
2019-02-19 13:10:55 -0800

    update starter h08

---
+++ h08/h08.cpp
@@ -0,0 +1,24 @@
+/**
+ *  @author Put your name here
+ *  @date Put the date here
+ *  @file h08.cpp
+ */
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+
+#include "h08.h"
+// Add your function implementation here
+
+
+///////////////// Student Testing /////////////////////////
+int run()
+{
+    cout << "Student tests" << endl;
+    return 0;
+}
