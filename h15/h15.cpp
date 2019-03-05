e61d813939e6f93a45bb3e997426d82f5ab2db7e
Stephen <occ.computerscience@gmail.com>
2019-03-05 13:31:09 -0800

    h15 starter code added

---
+++ h15/h15.cpp
@@ -0,0 +1,27 @@
+/**
+ *  @author Put your name here
+ *  @date Put the date here
+ *  @file h15.cpp
+ */
+#include <string>
+#include <stdexcept>
+// Other headers if necessary
+using namespace std;
+
+string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+
+#include "h15.h"
+
+// Add your code here
+
+
+
+
+
+/////////////// STUDENT TESTING ////////////////////
+#include <iostream>
+int run()
+{
+    cout << "Student testing" << endl;
+    return 0;
+}
