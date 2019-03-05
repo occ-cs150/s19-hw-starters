543da8af50387b7c942e25686061bba76550ba14
Stephen <occ.computerscience@gmail.com>
2019-03-05 13:35:39 -0800

    h16 starter code added

---
+++ h16/h16.cpp
@@ -0,0 +1,26 @@
+/**
+ *  @author Put your name here
+ *  @date Put the date here
+ *  @file h16.cpp
+ */
+#include <string>
+#include <iostream>
+using namespace std;
+
+string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+
+#include "h16.h"
+
+// Add your code here
+
+
+
+
+
+//////////////////////// STUDENT TESTING //////////////////////////
+
+int run()
+{
+    cout << "Add your own tests here" << endl;
+    return 0;
+}
