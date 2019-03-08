e71bfc042673b4c36eee3ed87596a892c9bc0705
Stephen <occ.computerscience@gmail.com>
2019-03-08 14:35:44 -0800

    h22 starters uploaded

---
+++ h22/h22.cpp
@@ -0,0 +1,26 @@
+/**
+    @file h22.cpp
+    @author your name here
+    @version what day and meeting time
+*/
+#include <string>
+#include <stdexcept>
+#include <iomanip>
+#include <sstream>
+using namespace std;
+
+string STUDENT = "WHO AM I?"; // Add your Canvas/occ-email ID
+#include "h22.h"
+
+// Add your implementation here
+
+
+
+
+/////////////// Student Tests ////////////////////////////
+#include <iostream>
+int run()
+{
+	cout << "Student testing" << endl;
+	return 0;
+}
---
+++ h22/h22.h
@@ -0,0 +1,23 @@
+/*
+ *	@file h22.h
+ *  @author Your name goes here
+ *  @version  what day and meeting time
+ */
+#ifndef H22_H_
+#define H22_H_
+
+#include <string>
+
+/////// Declare all of your classes here ////////////////
+/////// In this order: Worker, SalariedWorker, HourlyWorker
+class Worker
+{
+public:
+    // . . .
+private:
+	std::string name;
+	double rate;
+};
+
+
+#endif
